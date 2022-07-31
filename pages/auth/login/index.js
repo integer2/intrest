422b87766115582b2859666b9c6d30d1fd220a10
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-07-31 18:45:02 +0700

    update login page file

--- pages/auth/login/index.js
+++ pages/auth/login/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { AuthLayout } from '../../../components/layout';
-import LoginModule from '../../../modules/auth/login';
+import { AuthLayout } from '@/components/layout';
+import LoginModule from '@/modules/auth/login';
 
 const LoginPage = () => {
   return (
     <AuthLayout isBackButton>
-      <div className='w-full max-w-[554px]'>
+      <div className="w-full max-w-[554px]">
         <LoginModule />
       </div>
     </AuthLayout>
