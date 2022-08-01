0d011f7dd2eea486606e6b9483bf895303985890
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-08-01 21:52:57 +0700

    generate token after login

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,4 +1,5 @@
 import db from '@/libs/db';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(req, res) {
   try {
@@ -10,17 +11,26 @@ export default async function handler(req, res) {
 
     const { email, password } = req.body;
 
-    const result = await db.query(`CALL loginUser(?, ?)`, [
-      email,
-      password,
-    ]);
+    const result = await db.query(`CALL loginUser(?, ?)`, [email, password]);
 
-    console.log(result[0]);
+    const data = result[0][0][0];
+
+    const token = jwt.sign(
+      {
+        id: data.id,
+        email: data.email,
+      },
+      'secret',
+      {
+        expiresIn: '7d',
+      }
+    );
 
     res.status(200).json({
       message: 'Login successful',
-      data: result[0],
+      token,
     });
+    
   } catch (error) {
     return res.status(500).json({
       error: error.message,
