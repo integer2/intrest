83fc2cfe2389196d0e90f2733d6732aeb3f103aa
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-08-02 06:49:25 +0700

    auth

--- middlewares/authorization/index.js
+++ middlewares/authorization/index.js
@@ -1,7 +1,20 @@
-import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
 
-export default function authorization(req) {
-  const response = NextResponse.next();
-  response.cookies.set('token', '12345');
-  return response;
+export default function authorization(req, res) {
+  return new Promise((resolve, reject) => {
+    const { authorization } = req.headers;
+    if (!authorization)
+      return res.status(401).json({ message: 'Unauthorized' });
+    const authSplit = authorization.split(' ');
+    const [authType, authToken] = [authSplit[0], authSplit[1]];
+
+    if (authType !== 'Bearer')
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    return jwt.verify(authToken, 'secret', (err, decode) => {
+      if (err) return res.status(401).json({ message: 'Unauthorized' });
+
+      return resolve(decode);
+    });
+  });
 }
--- pages/api/post/create/index.js
+++ pages/api/post/create/index.js
@@ -1,9 +1,11 @@
-import authorization from "middlewares/authorization";
+import { authorization } from 'middlewares';
 
-export default function handler(req, res) {
-  if(req.method !== "POST")  return res.status(405).json({ error: "Method not allowed" });
-  authorization(req);
-  res.status(200).json({
-    message: "Halo Apa Kabar"
-  })
+export default async function handler(req, res) {
+  if (req.method !== 'POST')
+    return res.status(405).json({ error: 'Method not allowed' });
+  const auth = await authorization(req, res);
+
+  console.log(auth);
+
+  return res.status(200).json({ message: 'Create Post' });
 }
