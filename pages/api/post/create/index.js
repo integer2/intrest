0265afffee647f2434e4262a8424639c1246a36a
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-08-02 08:39:46 +0700

    update

--- pages/api/post/create/index.js
+++ pages/api/post/create/index.js
@@ -6,11 +6,13 @@ export default async function handler(req, res) {
   try {
     if (req.method !== 'POST')
       return res.status(405).json({ error: 'Method not allowed' });
-      
+
+    // Middlewares
     const auth = await authorization(req, res);
     const { fields, files } = await dataForm(req);
+
     const newPath = await moveFile(files.image);
-    
+
     const { id: author_id } = auth;
     const img_url = `/uploads/images/${newPath.split('/').pop()}`;
 
